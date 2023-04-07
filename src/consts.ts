f847eb623122e111b6435d71d1f8d60c323e8eb3
Weston Walker <walk8919@gmail.com>
2023-04-07 12:34:32 -0500

    added logging docs.

--- src/consts.ts
+++ src/consts.ts
@@ -7,12 +7,13 @@ export type Sidebar = Record<string, { text: string; link: string }[]>;
 export const SIDEBAR: Sidebar = {
     'Getting Started': [
         { text: 'Installation', link: 'installation' },
+        { text: 'Spark CLI', link: 'spark-cli' },
         { text: 'Directory Structure', link: 'directory-structure' },
         { text: 'Configuration', link: 'configuration' },
         { text: 'Database', link: 'database' },
         { text: 'Authentication', link: 'authentication' },
         { text: 'Authorization', link: 'authorization' },
-        { text: 'Spark CLI', link: 'spark-cli' },
+        { text: 'Logging', link: 'logging' },
     ],
     'Tutorials': [
         { text: 'Blog Tutorial', link: 'blog-tutorial' },

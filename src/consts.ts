10a1d9f5979bd44b933180bed66026fef39b5b42
Weston Walker <walk8919@gmail.com>
2023-04-19 13:48:29 -0500

    added introduction page.

--- src/consts.ts
+++ src/consts.ts
@@ -6,14 +6,20 @@ export const SITE = {
 export type Sidebar = Record<string, { text: string; link: string }[]>;
 export const SIDEBAR: Sidebar = {
     'Getting Started': [
+        { text: 'Introduction', link: 'introduction' },
         { text: 'Installation', link: 'installation' },
-        { text: 'Spark CLI', link: 'spark-cli' },
         { text: 'Directory Structure', link: 'directory-structure' },
+    ],
+    'The Basics': [
         { text: 'Configuration', link: 'configuration' },
         { text: 'Database', link: 'database' },
         { text: 'Authentication', link: 'authentication' },
         { text: 'Authorization', link: 'authorization' },
         { text: 'Logging', link: 'logging' },
+        { text: 'Events', link: 'events' },
+        { text: 'Tasks', link: 'tasks' },
+        { text: 'Scheduling', link: 'scheduling' },
+        { text: 'Spark CLI', link: 'spark-cli' },
     ],
     'Tutorials': [
         { text: 'Blog Tutorial', link: 'blog-tutorial' },
